412b69409d7a60ac172a775d97595247d5094187
KevinArdiprana9 <kevinardiprana9@gmail.com>
2025-07-30 16:40:02 +0700

    Fix namespace casing for UMKM model import in UMKMController

--- app/Http/Controllers/UMKMController.php
+++ app/Http/Controllers/UMKMController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
-use App\Models\UMKM;
+use App\Models\Umkm as UMKM;
 use Illuminate\Support\Facades\Storage;
 use App\Models\UmkmGallery;
 
